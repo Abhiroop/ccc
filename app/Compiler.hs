662f8ae7fb04782c194e77b35465fed92d4f5bb3
Abhiroop Sarkar <asiamgenius@gmail.com>
2018-02-24 23:48:28 +0000

    Addition of the stack

---
+++ app/Compiler.hs
@@ -0,0 +1,23 @@
+module Compiler where
+
+-- Hutton's Razor
+data Expr = Val Int
+          | Add Expr Expr
+          deriving Show
+
+eval :: Expr -> Int
+eval (Val n) = n
+eval (Add e1 e2) = eval e1 + eval e2
+
+-- Adding a stack
+type Stack = [Int]
+
+eval' :: Expr -> Stack -> Stack
+eval' (Val n) s   = push n s
+eval' (Add x y) s = add (eval' y (eval' x s))
+
+push :: Int -> Stack -> Stack
+push n s = n : s
+
+add :: Stack -> Stack
+add (m:n:s) = n + m :s
