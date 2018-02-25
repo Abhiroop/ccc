e1a90bc7e7685bb4f060ce4063c84b88d498ca56
Abhiroop Sarkar <asiamgenius@gmail.com>
2018-02-25 01:27:29 +0000

    Add continuations

--- app/Compiler.hs
+++ app/Compiler.hs
@@ -12,9 +12,9 @@ data Expr = Val Int
 -- Adding a stack
 type Stack = [Int]
 
-eval' :: Expr -> Stack -> Stack
-eval' (Val n) s   = push n s
-eval' (Add x y) s = add (eval' y (eval' x s))
+-- eval' :: Expr -> Stack -> Stack
+-- eval' (Val n) s   = push n s
+-- eval' (Add x y) s = add (eval' y (eval' x s))
 
 push :: Int -> Stack -> Stack
 push n s = n : s
@@ -24,3 +24,17 @@ add (m:n:s) = n + m :s
 
 eval :: Expr -> Int
 eval e = head (eval' e [])
+
+-- Adding a continuation
+
+type Cont = Stack -> Stack
+
+-- eval'' :: Expr -> Cont -> Cont
+-- eval'' e c s = c (eval' e s)
+
+eval'' :: Expr -> Cont -> Cont
+eval'' (Val n) c s   = c (push n s)
+eval'' (Add x y) c s = eval'' x (eval'' y (c.add)) s
+
+eval' :: Expr -> Cont
+eval' e = eval'' e id
