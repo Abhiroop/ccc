993cde6d4d095c071f4f220c7bf3c24956c64f22
Abhiroop Sarkar <asiamgenius@gmail.com>
2018-02-26 22:55:50 +0000

    Add defunctionalisation and executor from code to continuation

--- app/Compiler.hs
+++ app/Compiler.hs
@@ -32,9 +32,34 @@ type Cont = Stack -> Stack
 -- eval'' :: Expr -> Cont -> Cont
 -- eval'' e c s = c (eval' e s)
 
-eval'' :: Expr -> Cont -> Cont
-eval'' (Val n) c s   = c (push n s)
-eval'' (Add x y) c s = eval'' x (eval'' y (c.add)) s
+-- eval'' :: Expr -> Cont -> Cont
+-- eval'' (Val n) c s   = c (push n s)
+-- eval'' (Add x y) c s = eval'' x (eval'' y (c.add)) s
+
+-- eval' :: Expr -> Cont
+-- eval' e = eval'' e id
+
+-- Defunctionalisation
+
+haltC :: Cont
+haltC = id
+
+pushC :: Int -> Cont -> Cont
+pushC n c = c . push n
+
+addC :: Cont -> Cont
+addC c = c . add
 
 eval' :: Expr -> Cont
-eval' e = eval'' e id
+eval' e = eval'' e haltC
+
+eval'' :: Expr -> Cont -> Cont
+eval'' (Val n) c   = pushC n c
+eval'' (Add x y) c = eval'' x (eval'' y (addC c))
+
+data Code = HALT | PUSH Int Code | ADD Code deriving Show
+
+exec :: Code -> Cont
+exec HALT       = haltC
+exec (PUSH n c) = pushC n (exec c)
+exec (ADD c)    = addC (exec c)
